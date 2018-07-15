d432020df8896485a3b9298790b2d31cc7f97c3a
burry <burry439@gmail.com>
2018-07-15 16:43:16 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+const cors = require("cors");
+const Pusher = require("pusher");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+const pusher = new Pusher({
+    appId: `${process.env.PUSHER_APP_ID}`,
+    key: `${process.env.PUSHER_API_KEY}`,
+    secret: `${process.env.PUSHER_API_SECRET}`,
+    cluster: `${process.env.PUSHER_APP_CLUSTER}`,
+    encrypted: true
+});
+
+
+app.post("/message", function(req, res){
+    console.log(req.body)
+    pusher.trigger("events-channel", 'new-msg', {
+     chat:req.body 
+    });
+  
+});
+
+
+app.post("/update", function(req, res){
+    pusher.trigger("events-channel", "new-like", {
+     likes: {likes : req.body.likes}
+    });
+});
+
+
+
+
+
+    app.listen("8080");
+    console.log("Listening on localhost:8080");
